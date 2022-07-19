bfb1b008a3de517ae37b5e62901562938b71d3d4
hasanmohdkhan <hasanzian@gmail.com>
2022-07-19 17:33:10 +0530

    changes

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Created: 19/07/2022
@@ -12,9 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
 
-    @GetMapping("/hello")
-    public String getGreetings() {
+    @GetMapping(value = "/hello",produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public List<String> getGreetings() {
         System.out.println("called by API...");
-        return "hello";
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("World");
+        return list;
+
     }
 }
